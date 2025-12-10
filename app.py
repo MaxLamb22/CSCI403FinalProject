1d8336fee835235378a1c75f2634e1104b7d0228
Xander Lewis <xander_lewis@mines.edu>
2025-12-09 18:34:42 -0700

    Updated

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import getpass
-import pg8000
+import pg8000 # type: ignore
 import os
 from flask import Flask, render_template, request, flash, redirect, url_for
 from contextlib import contextmanager
@@ -65,7 +65,7 @@ def search():
             with get_db_connection() as db:
                 cursor = db.cursor()
                 cursor.execute("SET search_path TO maxwell_lamb")
-                query = "SELECT name, release_date, price FROM steam WHERE name ILIKE %s ORDER BY name"
+                query = "SELECT name, release_date, price FROM steam WHERE name ILIKE %s ORDER BY (positive_ratings-negative_ratings) DESC"
                 search_pattern = f"%{game_name}%"
                 cursor.execute(query, [search_pattern])
                 results = cursor.fetchall()
